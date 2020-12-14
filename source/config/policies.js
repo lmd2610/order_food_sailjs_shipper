2d0df8b80fcc780e55ad7be6284fd22512aaac11
Duc La <duclm@media-one.vn>
2020-12-15 02:49:54 +0700

    init commit

--- source/config/policies.js
+++ source/config/policies.js
@@ -22,30 +22,6 @@ module.exports.policies = {
   '*': true,
   'swagger': true,
   'swagger/swagger.json': true,
-  'user/get-all': 'customer',
-  'user/login': 'notAuthCustomer',
-  'user/register': 'notAuthCustomer',
-  'user/get-one': 'customer',
-  'user/update': 'customer',
-  'user/cancel-order':'customer',
-
-
-  'category/list-category': 'customer',
-
-  'order/calculate-bill': 'customer',
-  'order/create': 'customer',
-  'order/list-order-by-store': 'store',
-
-  'store/create': 'notAuthStore',
-  'store/get-all': 'customer',
-  'store/get-store-by-id': 'customer',
-  'store/login': 'notAuthStore',
-
-  'food/search-food': 'customer',
-  'food/get-food-by-storeid': 'customer',
-
-
-  'menu/get-menu-by-storeid': 'customer',
 
   'shipper/create':'notAuthShipper',
   'shipper/list-order':'shipper',
